9dd672d6857e9350db5e937a64e91d4a9a171627
Donovan Bailey <donovan.doubless@gmail.com>
2021-04-07 06:05:02 +0800

    Fix footer positioning & add browser tab icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pokedex from "../assets/img/pokedex.png";
+import Tooltip from "@material-ui/core/Tooltip";
 
 function Header({ loadHome }) {
 	return (
@@ -7,7 +8,9 @@ function Header({ loadHome }) {
 			<h1>
 				My<strong>Anime</strong>Database
 			</h1>
-			<img onClick={loadHome} src={pokedex} alt="Home" />
+			<Tooltip title="Home (Top Airing Anime)" placement="bottom">
+				<img onClick={loadHome} src={pokedex} alt="Home" />
+			</Tooltip>
 		</header>
 	);
 }
